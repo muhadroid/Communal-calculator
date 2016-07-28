887d33ab594bba856c976e16134bcbca8f5ebf68
aurtom <artemmikhalov@i.ua>
2016-07-28 14:05:58 +0300

    moved cost field and setter to abstract superclass

--- src/Calculators/Calculator.java
+++ src/Calculators/Calculator.java
@@ -1,5 +1,10 @@
 package Calculators;
 
 public abstract class Calculator {
+    protected double cost;
     public abstract double calculate(int i);
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
--- src/Calculators/WaterCalculator.java
+++ src/Calculators/WaterCalculator.java
@@ -1,13 +1,9 @@
 package Calculators;
 
 public class WaterCalculator extends Calculator {
-    private double cost;
     @Override
     public double calculate(int i) {
         return (double) i*cost;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
 }
